7f9ddab7aee64d6593e1e14116437fd4b2dc7cc9
Dânio Filho <danio.filho@navegarti.com.br>
2025-08-25 16:38:14 -0300

    fix: vite build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are using plugins like @vitejs/plugin-react or @vitejs/plugin-vue,
-  // make sure to include them here inside a `plugins` array.
-  // plugins: [react()],
-  base: '/aeons-end-card-generator/',
+  base: './',
 });
